ecb8b98a3385505b19ad785794f2af7702277b5b
frank2 <frank2@dc949.org>
2017-02-25 14:58:07 -0800

    include badaes.hpp

---
+++ src/include/badaes.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <badaes/cipher.hpp>
+#include <badaes/exception.hpp>
+#include <badaes/field.hpp>
+#include <badaes/key.hpp>
+#include <badaes/sbox.hpp>
+#include <badaes/state.hpp>
+#include <badaes/word.hpp>
